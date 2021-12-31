0b29c8ddd858658e431f57891b2a336364ff5855
omkarvaigankar8 <omkarvaigankar8@gmail.com>
2022-01-01 02:21:45 +0530

    working and UI completed

--- components/Filters/index.js
+++ components/Filters/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './filters.module.scss';
 import Select, { components } from 'react-select';
+import { Carrot } from '../Icons';
 const Filters = ({
 	product,
 	city,
@@ -13,16 +14,15 @@ const Filters = ({
 	setCityOptions,
 	sliderData,
 	sliderData2,
-	sliderData3
-
+	sliderData3,
 }) => {
 	// const checkOp
-		// const uniqProduct = [...new Set(productArr)];
-		// const uniqState = [...new Set(stateArr)];
-		// const uniqCity = [...new Set(cityArr)];
-	console.log('1',productOptions)
-	console.log('2',stateOptions)
-	console.log('3',cityOptions)
+	// const uniqProduct = [...new Set(productArr)];
+	// const uniqState = [...new Set(stateArr)];
+	// const uniqCity = [...new Set(cityArr)];
+	console.log('1', productOptions);
+	console.log('2', stateOptions);
+	console.log('3', cityOptions);
 
 	const productOptionList = productOptions.map((op) => {
 		return { value: `${op}`, label: `${op}` };
@@ -35,17 +35,18 @@ const Filters = ({
 	});
 
 	const customStyles = {
-		// indicatorSeparator: (styles) => ({ display: "none" }),
+		indicatorSeparator: (styles) => ({ display: 'none' }),
+		indicatorContainer: (styles) => ({ color: 'red', backgroundColor: 'red' }),
 		option: (provided, state) => ({
-		  ...provided,
-		  "&:hover": {
-			backgroundColor: "#232323",
-			color: "#ffffff",
-		  },
-	
-		  color: state.isSelected ? "white" : "white",
-		  backgroundColor: state.isSelected ? "#232323" : '#232323',
-		  //backgroundColor: state.isSelected ? "rgb(95, 50, 187, 10%)" : "white",
+			...provided,
+			'&:hover': {
+				backgroundColor: '#232323',
+				color: '#ffffff',
+			},
+
+			color: state.isSelected ? 'white' : 'white',
+			backgroundColor: state.isSelected ? '#232323' : '#232323',
+			//backgroundColor: state.isSelected ? "rgb(95, 50, 187, 10%)" : "white",
 		}),
 		// control: (base, state) => ({
 		//   ...base,
@@ -66,50 +67,63 @@ const Filters = ({
 		// 	borderColor: "#03658c",
 		//   },
 		// }),
-		control:(base,state) => ({
+		control: (base, state) => ({
 			...base,
-			color: state.isSelected ? "#fffffff" : "#ffffff",
-			backgroundColor: state.isSelected ? "#232323" : '#232323',
+			color: state.isSelected ? '#fffffff' : '#ffffff',
+			backgroundColor: state.isSelected ? '#232323' : '#232323',
+			width: '100%',
+			borderColor:'transparent'
 		}),
-	  };	
+		singleValue: (base, state) => ({
+			...base,
+			color: state.isSelected ? '#ffffff' : '#ffffff',
+			fontSize:'17px'
+		}),
+		menuList: (base, state) => ({
+			...base,
+			paddingTop: '0',
+			paddingBottom: '0',
+		}),
+	};
 	//   <components.DropdownIndicator>
-    //   <img src="https://via.placeholder.com/30x30"/>
-    // </components.DropdownIndicator>
+	//   <img src="https://via.placeholder.com/30x30"/>
+	// </components.DropdownIndicator>
 
 	return (
 		<div className={styles.filter_section}>
 			<h3>Filters</h3>
 			<Select
-			// components={{ DropdownIndicator }}
+				components={{ DropdownIndicator }}
 				className={styles.select}
-				placeholder="Product"
+				placeholder='Product'
 				onChange={(e) => {
 					console.log('CHANGE 1', e.value);
 					// setProductOptions(e.value)
-					sliderData(e.value)
+					sliderData(e.value);
 				}}
 				options={productOptionList}
 				styles={customStyles}
 			/>
-				<Select
+			<Select
+				components={{ DropdownIndicator }}
 				className={styles.select}
-				placeholder="State"
+				placeholder='State'
 				onChange={(e) => {
 					console.log('CHANGE 2', e.value);
 					// setStateOptions(e.value)
-					sliderData2(e.value)
-
+					sliderData2(e.value);
 				}}
 				options={stateOptionList}
 				styles={customStyles}
 			/>
-				<Select
+			<Select
+				components={{ DropdownIndicator }}
 				className={styles.select}
-				placeholder="City"
+				placeholder='City'
 				onChange={(e) => {
 					console.log('CHANGE 3', e.value);
 					// setCityOptions(e.value)
-					sliderData3(e.value)
+					sliderData3(e.value);
 				}}
 				styles={customStyles}
 				options={cityOptionList}
@@ -117,5 +131,11 @@ const Filters = ({
 		</div>
 	);
 };
-
+const DropdownIndicator = (props) => {
+	return (
+		<components.DropdownIndicator {...props}>
+			<Carrot />
+		</components.DropdownIndicator>
+	);
+};
 export default Filters;
---
+++ components/Icons/index.js
@@ -0,0 +1,7 @@
+export  const Carrot =() => {
+    return(
+        <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.09409 9.19L0.816466 0.0488873L11.3717 0.0488864L6.09409 9.19Z" fill="#A5A5A5"/>
+</svg>
+    )
+}
